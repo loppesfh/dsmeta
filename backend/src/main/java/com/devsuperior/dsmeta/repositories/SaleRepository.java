5d377a0fb3a1f7a672545192f3ac5ebdcad495a6
Loppes Filho <loppes@megganet.com.br>
2022-07-13 09:42:18 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("select obj from Sale obj where obj.date between :min and :max order by obj.amount desc")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
