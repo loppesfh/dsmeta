b2a963b9f90774310e69b833f4a056c81c115a3c
Loppes Filho <loppes@megganet.com.br>
2022-07-15 15:44:45 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import './styles.css';
 import icon from '../../assets/img/notification-icon.svg';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
   saleId: number
@@ -12,6 +13,7 @@ const NotificationButton = ({saleId} : Props) => {
   const handleClick = (id: number) => {
     axios.get(`${BASE_URL}/sales/${id}/notification`)
       .then(() => {
+        toast.info("SMS enviado com Sucesso!")
         console.log("enviada!")
       })
   }
